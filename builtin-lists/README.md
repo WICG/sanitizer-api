fa1fc8dfab8d14c84dfef75eefcb0cca1f1b167d
Daniel Vogelheim <vogelheim@chromium.org>
2023-07-14 17:41:58 +0200

    Remove outdated files.

--- builtin-lists/README.md
+++
@@ -1,4 +0,0 @@
-# Sanitizer API: Built-in Lists
-
-This directory is experimental and non-normative. The intent is to build up the
-Sanitizer API's built-in lists and configurations.
--- builtin-lists/sort.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for list in {elements,attributes}-*-{known,baseline,default}.txt; do
-  echo $list:
-  OUT=$(mktemp)
-  grep "^#" $list > $OUT
-  grep -v "^#" $list | sort >> $OUT
-  diff $list $OUT
-  cp $OUT $list
-done
